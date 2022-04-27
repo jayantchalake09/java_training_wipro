eb7955aa76dd5206ae6e9cb56ae898c303bfd605
jayantchalake09 <104410024+jayantchalake09@users.noreply.github.com>
2022-04-27 11:58:09 +0530

    Add files via upload

---
+++ Hello_world.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Hello_world {
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		//System.out.print("Hello World");
+		System.out.println("Jayant Pandurang Chalake");
+		System.out.println("Kolhapur");
+	
+		
+	}
+
+}
